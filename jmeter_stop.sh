517ce934b3aa47c4dfe22f63fecc2a3f6614a4fd
Lin, Nathan <nathan.lin@garmin.com>
2021-08-10 15:47:14 +0800

    add setup-db stage

--- jmeter_stop.sh
+++ jmeter_stop.sh
@@ -2,15 +2,9 @@
 #Script writtent to stop a running jmeter master test
 #Kindly ensure you have the necessary kubeconfig
 
-#编写脚本来停止运行的 jmeter master 测试
-#请确保你有必要的 kubeconfig
-
 working_dir=`pwd`
 
-#获取 namesapce 变量
-tenant=`awk '{print $NF}' $working_dir/tenant_export`
-
-master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
+master_pod=`kubectl get po --kubeconfig=$KUBE_CONFIG -n jmeter | grep jmeter-master | awk '{print $1}'`
 
 #kubectl -n $tenant exec -ti $master_pod bash /jmeter/apache-jmeter-5.0/bin/stoptest.sh
 
